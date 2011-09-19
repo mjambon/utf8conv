f975501f3645ab491b6f9add2806ebd990a2d03c
Martin Jambon <martin@mjambon.com>
2011-09-19 13:47:35 -0700

    Documentation changes

--- utf8conv.mli
+++ utf8conv.mli
@@ -2,13 +2,25 @@
    Conversion of ISO-8859-1/Windows-1252 text into UTF-8.
 
    This is a minimalistic module that does not require any configuration file
-   and does not depend on any third-party library.
-
-   @param pos position of the beginning of the input substring.
-              The default is 0, the beginning of the string.
-   @param len length of the input substring.
-              The default length extends until the end of the string.
+   and only depends on OCaml's standard library.
 *)
+
+val utf8_of_windows1252 :
+  ?pos: int ->
+  ?len: int ->
+  ?undefined: (char -> string) ->
+  string -> string
+  (** Converts an ASCII, ISO-8859-1 or Windows-1252 string or substring
+      into a UTF-8 string.
+
+      @param pos position of the beginning of the input substring.
+                 The default is 0, the beginning of the string.
+      @param len length of the input substring.
+                 The default length extends until the end of the string.
+      @param undefined function for converting invalid bytes. The default
+                 is to raise a [Failure] exception with a useful
+                 error message.
+  *)
 
 val is_ascii : ?pos: int -> ?len: int -> string -> bool
   (** Returns true if and only if the given string or substring
@@ -16,9 +28,8 @@ val is_ascii : ?pos: int -> ?len: int -> string -> bool
 
 val is_iso88591 : ?pos: int -> ?len: int -> string -> bool
   (** This function returns true if and only if the given string or substring
-      contains only legal ISO-8859-1-encoded characters defined as the union
-      of all 7-bit characters (0-127)
-      and all 8-bit printable characters (160-255).
+      contains only legal ISO-8859-1-encoded characters
+      (all printable characters plus all ASCII control characters).
 
       ISO-8859-1 is the default encoding assumed by HTTP 1.1.
       However, it is often confused with Windows-1252 which is a superset
@@ -31,27 +42,15 @@ val is_iso88591 : ?pos: int -> ?len: int -> string -> bool
 
 val is_windows1252 : ?pos: int -> ?len: int -> string -> bool
   (** This function returns true if and only if the given string or substring
-      contains only legal Windows-1252-encoded characters defined as the union
-      of all 7-bit characters (0-127)
-      and all 8-bit printable characters (160-255).
+      contains only legal Windows-1252-encoded characters
+      (all printable characters plus all ASCII control characters).
 
       Windows-1252 is also known as CP-1252. It adds a few printable
-      characters to ISO-8859-1 in the 128-159 range.
+      characters to ISO-8859-1 in the 128-159 range. Please note that
+      the Unicode code points of these extra characters is not equal
+      to their byte value.
 
       See also [is_iso88591].
-  *)
-
-val utf8_of_windows1252 :
-  ?pos: int ->
-  ?len: int ->
-  ?undefined: (char -> string) ->
-  string -> string
-  (** Converts an ASCII, ISO-8859-1 or Windows-1252 string or substring
-      into a UTF-8 string.
-
-      @param undefined function for converting invalid bytes. The default
-                       is to raise a [Failure] exception with a useful
-                       error message.
   *)
 
 val escape :
